d788b3eed1378ba14821720e027e14531be4ae0b
yamato1491038 <67918886+yamato1491038@users.noreply.github.com>
2021-04-19 15:04:48 +0900

    layout_fix

---
+++ app/Http/Controllers/MyImageController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MyImageController extends Controller
+{
+    function upload(Request $request){
+        
+    }
+}
---
+++ app/Models/MyImage.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MyImage extends Model
+{
+    use HasFactory;
+
+    protected $table = "my_images";
+    protected $fillable = ["file_name", "file_path" ];
+
+}
